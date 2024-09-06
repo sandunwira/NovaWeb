05e09c63fbc98aaa0fc5de79534144faea86eb6d
sandunwira <79461263+sandunwira@users.noreply.github.com>
2024-09-06 11:05:21 +0530

    Initialized the Web

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+var favicon = require('serve-favicon');
+
+
+// Static Files Start
+app.use(express.static(path.join(__dirname, 'public')));
+// Static Files End
+
+
+// Favicon Start
+app.use(favicon(path.join(__dirname, 'public', 'assets', 'favicon.png')));
+// Favicon End
+
+
+// URL Routes Start
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+// URL Routes End
+
+
+// Error Handling Start
+app.use((req, res) => {
+	res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+// Error Handling End
+
+
+// Port Configuration Start
+app.listen(port, () => {
+	console.log(`Server listening at http://localhost:${port}`);
+});
+// Port Configuration End
